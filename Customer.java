6da31ba504ce489c38c046066b890fb521e3ad76
Jiang <jiangjon27@7hills.org>
2025-09-22 09:31:18 -0400

    Made Customer class

---
+++ Customer.java
@@ -0,0 +1,35 @@
+public class Customer {
+	
+	public static void main (String[] args) {
+		//Instance
+		double favor;
+		
+		minFav = 0;
+		maxFav = 50;
+		
+		//Attributes
+		
+		
+		//Methods
+		public favoritability() {
+				//affected by the weather and temperature and price and recipe/how tasty it is
+				/*
+				 * 
+				 * 
+				 * */
+		}
+		
+		public decideToBuy {
+			if favor >= 70 {
+				int randFavNum = (int)(Math.random() * (maxFav - minFav + 1)) + minFav;
+				if randFavNum >= 25 {
+					cash = cash + Price;
+			else {
+				cash = cash
+			}
+		}
+		
+		
+	}
+}
+
