ed6032fd44d2253e72bd634d504f07694b6df014
Jiang <jiangjon27@7hills.org>
2025-09-22 08:50:07 -0400

    Smth

---
+++ LemonadeStand.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class LemonadeStand {
+	
+	public static void main (String[] args) {
+		//Instance Variables
+		int recipe;
+		double cash;
+		double icePrice = 0.50;
+		double lemonPrice = 1.50;
+		double cupPrice = 1.00;
+		double sugarPrice = 1.50;
+		
+		//Attributes
+		String[] inventory = {"ice", "lemon", "cup", "sugar"};
+		
+		
+		//Methods
+		public setRecipe(inventory) {
+			Scanner iceAmt = new Scanner(System.in);
+			System.out.println("Please enter how much ice you want in the recipe...");
+			Scanner lemonAmt = new Scanner(System.in);
+			System.out.println("Please enter how much lemons you want in the recipe...");
+			Scanner cupAmt = new Scanner(System.in);
+			System.out.println("Please enter how much cups you want in the recipe...");
+			Scanner sugarAmt = new Scanner(System.in);
+			System.out.println("Please enter how much suagr you want in the recipe...");
+			recipe = (iceAmt + lemonAmt + cupAmt + sugarAmT);
+			//Not finished yet with recipe work in progress
+	}
+		public setPrice() {
+			Scanner price = new Scanner(System.in);
+			System.out.println("Set the price of your cups");
+		}
+		public getWeather() {
+			
+		}
+		public buySupplies(cash) {
+			
+		}
+}
+
