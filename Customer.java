424785308e950c5ff073e1d4b0f6c37f890be943
Bob283527 <jiangjon27@7hills.org>
2025-09-23 15:58:04 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -6,13 +6,12 @@ public class Customer {
     private final int minFav = 0;
     private final int maxFav = 50;
 
-    // Placeholder values
     private double price;
     private double cash;
 
-    // Determine how favorable the customer finds your lemonade
+    // find favorable the customer finds your lemonade
     public void calculateFavorability(String weather, int temperature, double price, int recipeQuality) {
-        // Dummy logic: favorability depends on weather and recipe quality
+        // favorability depends on weather and recipe quality
         favorability = 50;
 
         if (weather.equals("Sunny")) {
