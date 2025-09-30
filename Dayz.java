745d7a0212466cac3bf320d009e70000797a38d3
Bob283527 <jonjiang27@7hills.org>
2025-09-30 09:23:56 -0400

    still figuring out hoe to sim days and customers

---
+++ Dayz.java
@@ -0,0 +1,22 @@
+public class Dayz {
+	
+	//idk if this gon stay a var or not
+	int days;
+	int customers = 0;
+	
+	
+	
+	
+	public simCustomers() {
+		for(customers < 51) {
+			/*
+			 I want a way to use my Customer class for the favoritability method and whether they decide to buy or not
+			 then +=1 to the var customers so it loops for every customer
+			 then I want to add to the final cash the price of the lemonadeCup
+			 
+			 
+			 
+			 */
+	}
+}
+
